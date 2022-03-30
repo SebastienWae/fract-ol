edc13d2fca441fd0ffe98fcb4ce8996ec8e93377
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-03-30 09:32:41 +0200

    remove unused var

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: swaegene <swaegene@student.42.fr>          +#+  +:+       +#+        */
+/*   By: seb <seb@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 13:38:15 by swaegene          #+#    #+#             */
-/*   Updated: 2022/03/28 13:38:21 by swaegene         ###   ########.fr       */
+/*   Updated: 2022/03/30 09:24:45 by seb              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,8 @@
 int	main(void)
 {
 	void	*mlx;
-	void	*mlx_win;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_new_window(mlx, 1920, 1080, "Hello world!");
 	mlx_loop(mlx);
 }
